4a7daf02afd8828f32aa8a2423af2a84117e173a
Baliram Gupta <baliramgupta@Balirams-MacBook-Air.local>
2023-02-26 16:26:56 +0530

    login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,76 @@
-import React from 'react';
+import React from "react";
+import { Input, FormGroup, InputGroup, InputGroupText, Button } from "reactstrap";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PersonIcon from '@mui/icons-material/Person';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import PeopleIcon from '@mui/icons-material/People';
+
 import './Login.scss';
 
-function Login() {
-    return (
-        <div>Login</div>
+const Login = () => {
+    return(
+    <div className="login">
+        {/* <div className="form">
+            <h3 className="title">Login</h3>
+            <input type="button" value="" />
+        </div> */}
+        <FormGroup className="form">
+            <h3 className="title">LOGIN</h3>
+
+            <InputGroup className="input">
+                <InputGroupText className="icon">
+                    <PeopleIcon/>
+                </InputGroupText>
+                <Input
+                    type="select"
+                    // value="role"
+                    placeholder="Select Your Role"
+                    onChange={()=>{}}
+                    className="input-type role"
+                >
+                    <option value="lab">Lab</option>
+                    <option value="doctor">Doctor</option>
+                    <option value="admin">Admin</option>
+                </Input>
+            </InputGroup>
+
+            <InputGroup className="input">
+                <InputGroupText className="icon">
+                    {/* <FontAwesomeIcon icon="fa-solid fa-user" /> */}
+                    <PersonIcon/>
+                </InputGroupText>
+                <Input
+                    type="text"
+                    // value="username"
+                    placeholder="Enter Your Username"
+                    onChange={()=>{}}
+                    className="input-type"
+                >
+                    {/* <FontAwesomeIcon icon="fa-solid fa-user" /> */}
+                    Username
+                </Input>
+            </InputGroup>
+
+            <InputGroup className="input">
+                <InputGroupText className="icon">
+                    <VpnKeyIcon/>
+                </InputGroupText>
+                <Input
+                    type="password"
+                    // value="password"
+                    placeholder="Enter Your Password"
+                    onChange={()=>{}}
+                    className="input-type"
+                >
+                    Password
+                </Input>
+            </InputGroup>
+
+            <Button className="btn">
+                LOGIN
+            </Button>
+        </FormGroup>
+    </div>
     )
 }
 
