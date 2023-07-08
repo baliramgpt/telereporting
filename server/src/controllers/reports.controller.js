c4d0a8217d89117a9581a200d717df6a2d97374b
baliramgpt <baliramgpt@gmail.com>
2023-07-08 16:11:10 +0530

    reports api fix

--- server/src/controllers/reports.controller.js
+++ server/src/controllers/reports.controller.js
@@ -1,71 +1,51 @@
-const { reports } = require('../models/reports.model');
+const reports = require('../models/reports.model');
 
-async function getAllReports() {
-    return await reports.find({});
+let DEFAULT_REG_NO = 1000;
+
+async function getLatestRegNo(){
+    const latestReport = await reports
+        .findOne()
+        .sort('-regNo');
+
+    if(!latestReport){
+        return DEFAULT_REG_NO;
+    }
+
+    return latestReport.regNo;
 }
 
-function addNewReport(req, res) {
-    const report = req.body;
+async function getAllReports(req, res) {
+    return res.status(200).json(await reports.find({}, { '_id': 0, '__v': 0 }));
+}
 
-    // if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target){
-    //     return res.status(400).json({
-    //         error: "Missing requires launch property",
-    //     });
-    // }
+async function addNewReport(req, res) {
+    const newRegNo = await getLatestRegNo() + 1;
+
+    const newReport = Object.assign(req.body, {
+        regNo: newRegNo,
+    })
 
     try {
-        report.testDate = new Date(report.testDate);
-        if (isNaN(report.testDate)) {
+        newReport.testDate = new Date(newReport.testDate);
+        if (isNaN(newReport.testDate)) {
             return res.status(400).json({
                 error: 'Invalid test date',
             })
         }
-
-        latestRegNo += 1;
-        reports.set(latestRegNo, Object.assign(report));
+        // reports.set(latestRegNo, Object.assign(report));
+        // await reports.updateOne({}, {$set: newReport});
+        await reports.findOneAndUpdate({
+            regNo: newRegNo,
+        }, newReport, {
+            upsert: true,
+        });
     }
     catch (err) {
         console.error(err)
+        return;
     }
-    return res.status(201).json(report);
+    return res.status(201).json(newReport);
 }
-
-// function httpGetAllLaunches(req,res) {
-//     return res.status(200).json(getAllLaunches());
-// }
-
-// function httpAddNewLaunch(req, res) {
-//     const launch = req.body;
-
-//     if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target){
-//         return res.status(400).json({
-//             error: "Missing requires launch property",
-//         });
-//     }
-
-//     launch.launchDate = new Date(launch.launchDate);
-//     if(isNaN(launch.launchDate)){
-//         return res.status(400).json({
-//             error: 'Invalid launch date',
-//         })
-//     }
-
-//     addNewLaunch(launch);
-//     return res.status(201).json(launch);
-// }
-
-// function httpAbortLaunch(req, res){
-//     const launchId = Number(req.params.id);
-
-//     if(!existsLaunchWithId(launchId)){
-//         return res.status(404).json({
-//             error: 'Launch not found',
-//         })
-//     }
-
-//     const aborted = abortsLaunchById(launchId);
-//     return res.status(200).json(aborted);
-// }
 
 module.exports = {
     getAllReports,
